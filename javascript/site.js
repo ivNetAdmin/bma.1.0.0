1ee9e14f9b25ba1b70ec533542d5ff33c325b3c5
ivNetAdmin <bp@ivnet.co.uk>
2019-04-08 16:28:52 +0100

    Initial commit

---
+++ javascript/site.js
@@ -0,0 +1,20 @@
+$(function() { 
+   new universalParallax().init({
+      speed: 8.0
+  });
+}); 
+
+$(window).scroll(function() {    
+   var scroll = $(window).scrollTop();
+
+   if (scroll >= 180) {
+    //  alert("cakes");
+      $('.navbar-brand img').addClass('shrink-navbar-brand');
+      $('.nav-logo').addClass('shrink-nav-logo');
+   } else {        
+      $('.nav-logo').removeClass('shrink-nav-logo');
+       $('.navbar-brand img').removeClass('shrink-navbar-brand');
+  }
+});
+
+nav-logo
