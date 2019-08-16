60e5173ad2bf390284bd3cd1d11eb62e8657c179
ivNetAdmin <bp@ivnet.co.uk>
2019-08-16 14:03:02 +0100

    Gone Live

--- javascript/site.js
+++ javascript/site.js
@@ -18,9 +18,40 @@ $(function() {
    $(".service-block").fadeIn(500);
 });
 
+
+$('#email-button').click(function() {
+
+   const html = "<div>" +
+       "<p>Name: " + $('.txt[name="name"]').val() + "</p>" +
+       "<p>Tel: " + $('.txt[name="mob"]').val() + "</p>" +
+       "<p>Email: " + $('.txt[name="email"]').val() + "</p>" +
+       "<p>Notes: " + $('.txt_3').val() + "</p>" +
+       "</div>"
+
+   Email.send({      
+       SecureToken : "080eef13-07c8-4f65-92dc-083189461630",
+       To : ['e.egan@benchmarkaccountants.co.uk','d.tate@benchmarkaccountants.co.uk','bp@ivnet.co.uk'],
+       From : "website@ivnet.co.uk",
+       Subject : "Website Contact",
+       Body : html
+   }).then(
+     message => addEmailMessage(message)
+   );
+
+   $(".txt").each(function() {
+       $(this).val('');
+   });
+
+   $(".txt_3").val('');
+});
+
+
 }); 
 
-
+function addEmailMessage(msg)
+{
+  $('#emailok').text('Thank you for the enquiry, the team will get back to you shortly');
+}
 
 $(window).scroll(function() {    
    var scroll = $(window).scrollTop();
